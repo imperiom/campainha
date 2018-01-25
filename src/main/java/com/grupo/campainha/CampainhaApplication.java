750e763a9fca4a146d868b50cba362ac4b0e512e
Rodrigo Montanholi <rodrigo@montanholi.com>
2018-01-25 19:11:55 -0200

    subindo o spring boot

---
+++ src/main/java/com/grupo/campainha/CampainhaApplication.java
@@ -0,0 +1,12 @@
+package com.grupo.campainha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampainhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampainhaApplication.class, args);
+	}
+}
